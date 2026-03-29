f1f90277505cde3ce8c9352bee713eae16ecccc2
mgcf27 <190297408+mgcf27@users.noreply.github.com>
2026-03-29 12:53:19 -0300

    Activate test profile on ApplicationContext test

--- src/test/java/com/miguel/assistencesystem/AssistencesystemApplicationTests.java
+++ src/test/java/com/miguel/assistencesystem/AssistencesystemApplicationTests.java
@@ -2,8 +2,10 @@ package com.miguel.assistencesystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AssistencesystemApplicationTests {
 
 	@Test
